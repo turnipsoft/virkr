799112aad4ed5b537cbe6a6555d5e04e02b94d57
sorenhartvig <soeren.hartvig@gmail.com>
2017-05-19 09:09:00 +0200

    start

---
+++ src/main/groovy/dk/ts/virkr/main/VirkrApplication.groovy
@@ -0,0 +1,15 @@
+package dk.ts.virkr.main
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+/**
+ * Created by sorenhartvig on 19/05/2017.
+ */
+@SpringBootApplication
+class VirkrApplication {
+
+    public static void main(String []args) {
+        SpringApplication.run(VirkrApplication.class, args)
+    }
+}
