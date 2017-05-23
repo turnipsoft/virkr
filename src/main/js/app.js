0cd6a3aa0573dd33c2bc71616b27ff7f7840dd8f
sorenhartvig <soeren.hartvig@gmail.com>
2017-05-23 10:13:10 +0200

    recharts er erstattet med charts.js 2 react plugin, søgefelt ændret til et bedre felt der reagerer på enter. virkr.dk bringer til hovedside

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,203 +1,329 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import jQuery from 'jquery';
-import { LineChart, Line, CartesianGrid, YAxis, XAxis, Tooltip, Legend} from 'recharts';
+//import { LineChart, Line, CartesianGrid, YAxis, XAxis, Tooltip, Legend} from 'recharts';
+import {Line} from 'react-chartjs-2';
 
 class Virksomhed extends React.Component {
 
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.state = {
-      cvrnummer : '',
-      regnskaber : []
-    };
-  }
-
-  render() {
-    return (<div className="virksomhed">
-              <div className="row">
-                <div className="col">
-                  <h1 id="virkr-header">Virkr</h1>
-                  <p id="virkr-tagline">Nøgletal om virksomheder</p>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col">
-                <CvrSoegebox opdaterCvr={this._opdaterCvrNummer.bind(this)} />
-                </div>
-              </div>
-              <div className="row">
-                <div className="col">
-                  <CvrVisning cvrnummer={this.state.cvrnummer}
-                    regnskaber={this.state.regnskaber} />
-                </div>
-              </div>
-            </div>);
-  }
-
-  _opdaterCvrNummer(cvrnr) {
-    this.setState( {
-      cvrnummer : cvrnr
-    });
-
-    if (cvrnr && cvrnr.length==8) {
-      this._hentNoegletal(cvrnr);
-    } else {
-      this.setState({
-        regnskaber: []
-      })
+        this.state = {
+            cvrnummer : '',
+            regnskaber : []
+        };
     }
-  }
 
-  _hentNoegletal(cvrnummer) {
-      jQuery.ajax({
-        method: 'GET',
-        url: '/regnskab/'+cvrnummer,
-        success : (rs) => {
-          this.setState({
-            regnskaber : rs.regnskabsdata
-          })
-        },
-        error : (xhr, msg, err) => {
-          alert(msg + err + xhr);
+    render() {
+        return (<div className="virksomhed">
+          <div className="row">
+            <div className="col">
+              <h1 id="virkr-header"><a href="/" id="virk-header-a">Virkr</a></h1>
+              <p id="virkr-tagline">Nøgletal om virksomheder</p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <CvrSoegebox opdaterCvr={this._opdaterCvrNummer.bind(this)} />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <CvrVisning cvrnummer={this.state.cvrnummer}
+                          regnskaber={this.state.regnskaber} />
+            </div>
+          </div>
+        </div>);
+    }
+
+    _opdaterCvrNummer(cvrnr) {
+        this.setState( {
+            cvrnummer : cvrnr
+        });
+
+        if (cvrnr && cvrnr.length==8) {
+            this._hentNoegletal(cvrnr);
+        } else {
+            this.setState({
+                regnskaber: []
+            })
         }
-      });
-  }
+    }
+
+    _hentNoegletal(cvrnummer) {
+        jQuery.ajax({
+            method: 'GET',
+            url: '/regnskab/'+cvrnummer,
+            success : (rs) => {
+                this.setState({
+                    regnskaber : rs.regnskabsdata
+                })
+            },
+            error : (xhr, msg, err) => {
+                alert(msg + err + xhr);
+            }
+        });
+    }
 }
 
 class CvrSoegebox extends React.Component {
 
-  render() {
-    return (<div className="cvr-input">
-      <form>
-        <div className="input-group input-group-lg">
-          <span className="input-group-addon" id="sizing-addon1">CVR</span>
-          <input type="text" className="form-control input-lg"
-            id="cvr" ref={c => this._cvr= c} placeholder="Udfyld CVR Nummer"
-            aria-describedby="sizing-addon1"
-            onChange={this._opdaterCvrNummer.bind(this)} />
-        </div>
+    render() {
+        return (<div className="cvr-input">
+          <form onSubmit={this._opdaterCvrNummer.bind(this)}>
+            <div className="input-group">
+              <input type="text" ref={c => this._cvr= c}
+                     className="form-control" placeholder="Indtast CVR-Nummer"
+                     maxLength="8" id="txtSearch"/>
+              <div className="input-group-btn">
+                <button className="btn btn-primary" type="submit">
+                  <span className="fa fa-search"></span>
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>);
+    }
 
-      </form>
-    </div>);
-  }
-
-  _opdaterCvrNummer() {
-    this.props.opdaterCvr(this._cvr.value);
-  }
+    _opdaterCvrNummer(e) {
+        e.preventDefault();
+        const iv = parseInt(this._cvr.value);
+        if (!Number.isInteger(iv)) {
+            alert(this._cvr.value + " er ikke et CVR-Nummer")
+            return;
+        }
+        if (this._cvr.value && this._cvr.value.length!=8) {
+            alert("Et CVR-Nummer består altid af 8 cifre");
+            return;
+        }
+        this.props.opdaterCvr(this._cvr.value);
+    }
 }
 
 class CvrVisning extends React.Component {
 
-  componentDidUpdate() {
+    componentDidUpdate() {
 
-  }
-
-  render() {
-    const r = this._getRegnskabstal();
-    const c = this._getLineChart();
-
-    return(<div className="virksomhedsdata">
-      {c}
-      {r}
-    </div>);
-  }
-
-  _getRegnskabstal() {
-    return this.props.regnskaber.slice().reverse().map((regnskab) => {
-      return <Regnskabstal
-               id={regnskab.id}
-               aar={regnskab.aar}
-               startDato={regnskab.startDato}
-               slutDato={regnskab.slutDato}
-               bruttofortjeneste={regnskab.bruttofortjeneste}
-               driftsresultat={regnskab.driftsresultat}
-               egenkapital={regnskab.egenkapital}
-               aaretsresultat={regnskab.aaretsresultat}
-               resultatfoerskat={regnskab.resultatfoerskat}
-               skatafaaretsresultat={regnskab.skatafaaretsresultat}
-               gaeldsforpligtelser={regnskab.gaeldsforpligtelser}
-               key={regnskab.startDato} />
-    });
-  }
-
-  _getLineChart() {
-
-    if (this.props.regnskaber.length>0) {
-      return (
-        <LineChart width={1000} height={300} data={this.props.regnskaber}
-            margin={{top: 5, right: 30, left: 100, bottom: 20}}>
-            <XAxis dataKey="aar"/>
-            <YAxis/>
-            <CartesianGrid strokeDasharray="3 3"/>
-            <Tooltip/>
-            <Legend />
-            <Line type="monotone" dataKey="aaretsresultat" name="Årets resultat" stroke="#8884d8" activeDot={{r: 8}}/>
-            <Line type="monotone" dataKey="bruttofortjeneste" name="Bruttofortjeneste" stroke="#82ca9d" />
-            <Line type="monotone" dataKey="egenkapital" name="Egenkapital" stroke="#113d84" />
-      </LineChart>
-      );
     }
-    return;
-  }
+
+    render() {
+        const r = this._getRegnskabstal();
+        const c = this._getChartJsLineChart();
+
+        return(<div className="virksomhedsdata">
+            {c}
+          <br/>
+          <br/>
+            {r}
+        </div>);
+    }
+
+    _getRegnskabstal() {
+        return this.props.regnskaber.slice().reverse().map((regnskab) => {
+            return <Regnskabstal
+                id={regnskab.id}
+                aar={regnskab.aar}
+                startDato={regnskab.startDato}
+                slutDato={regnskab.slutDato}
+                bruttofortjeneste={regnskab.bruttofortjeneste}
+                driftsresultat={regnskab.driftsresultat}
+                egenkapital={regnskab.egenkapital}
+                aaretsresultat={regnskab.aaretsresultat}
+                resultatfoerskat={regnskab.resultatfoerskat}
+                skatafaaretsresultat={regnskab.skatafaaretsresultat}
+                gaeldsforpligtelser={regnskab.gaeldsforpligtelser}
+                pdfUrl={regnskab.pdfUrl}
+                key={regnskab.startDato} />
+        });
+    }
+
+    _getChartJsLineChart() {
+
+        if (this.props.regnskaber.length>0) {
+
+            Chart.defaults.global.scaleLabel = function(label) {
+                return label.value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+            };
+
+            Chart.defaults.global.multiTooltipTemplate = function(label){
+                return label.datasetLabel + ': ' + label.value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+            };
+
+            const options = {
+                tooltips: {
+                    enabled: true,
+
+                    callbacks: {
+                        label: function (tooltipItems, data) {
+                            return data.datasets[tooltipItems.datasetIndex].label + ' : ' + tooltipItems.yLabel.toLocaleString().replace(/,/g ,".");
+                        }
+                    }
+                },
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            // Create scientific notation labels
+                            callback: function(value) {
+                                return value.toLocaleString().replace(/,/g ,".");
+                            }
+                        }
+                    }]
+                }
+            }
+
+            const aarLabels = this.props.regnskaber.map((regnskab) => {
+                return regnskab.aar
+            });
+
+            const bruttofortjenester = this.props.regnskaber.map((regnskab) => {
+                return regnskab.bruttofortjeneste
+            });
+
+            const egenkapitaler = this.props.regnskaber.map((regnskab) => {
+                return regnskab.egenkapital;
+            });
+
+            const aaretsresultater = this.props.regnskaber.map((regnskab) => {
+                return regnskab.aaretsresultat;
+            });
+
+            const data = {
+                labels: aarLabels,
+                datasets: [
+                    {
+                        label: "Bruttofortjeneste",
+                        fill: false,
+                        lineTension: 0.1,
+                        backgroundColor: "rgba(75,192,192,0.4)",
+                        borderColor: "rgba(75,192,192,1)",
+                        borderCapStyle: 'butt',
+                        borderDash: [],
+                        borderDashOffset: 0.0,
+                        borderJoinStyle: 'miter',
+                        pointBorderColor: "rgba(75,192,192,1)",
+                        pointBackgroundColor: "#fff",
+                        pointBorderWidth: 1,
+                        pointHoverRadius: 5,
+                        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                        pointHoverBorderColor: "rgba(220,220,220,1)",
+                        pointHoverBorderWidth: 2,
+                        pointRadius: 1,
+                        pointHitRadius: 10,
+                        data: bruttofortjenester,
+                        spanGaps: false
+                    },
+                    {
+                        label: "Egenkapital",
+                        fill: false,
+                        lineTension: 0.1,
+                        backgroundColor: "#113d84",
+                        borderColor: "#113d84",
+                        borderCapStyle: 'butt',
+                        borderDash: [],
+                        borderDashOffset: 0.0,
+                        borderJoinStyle: 'miter',
+                        pointBorderColor: "#113d84",
+                        pointBackgroundColor: "#fff",
+                        pointBorderWidth: 1,
+                        pointHoverRadius: 5,
+                        pointHoverBackgroundColor: "#113d84",
+                        pointHoverBorderColor: "#113d84",
+                        pointHoverBorderWidth: 2,
+                        pointRadius: 1,
+                        pointHitRadius: 10,
+                        data: egenkapitaler,
+                        spanGaps: false
+                    },
+                    {
+                        label: "Årets resultat",
+                        fill: false,
+                        lineTension: 0.1,
+                        backgroundColor: "#8884d8",
+                        borderColor: "#8884d8",
+                        borderCapStyle: 'butt',
+                        borderDash: [],
+                        borderDashOffset: 0.0,
+                        borderJoinStyle: 'miter',
+                        pointBorderColor: "#8884d8",
+                        pointBackgroundColor: "#fff",
+                        pointBorderWidth: 1,
+                        pointHoverRadius: 5,
+                        pointHoverBackgroundColor: "#8884d8",
+                        pointHoverBorderColor: "#8884d8",
+                        pointHoverBorderWidth: 2,
+                        pointRadius: 1,
+                        pointHitRadius: 10,
+                        data: aaretsresultater,
+                        spanGaps: false
+                    }
+                ]};
+
+            return(
+                <div>
+                  <h2>Nøgletal for virksomheden</h2>
+                  <Line data={data} options={options} />
+                </div>
+            );
+        }
+    }
 
 }
 
 class Regnskabstal extends React.Component {
 
-  render() {
-    return(
-      <div className="card">
-        <div className="card-header" id="regnskab-header">
-          <b>{this.props.aar}</b> <span className="small">({this.props.startDato} - {this.props.slutDato})</span>
-        </div>
-        <div className="card-block">
-          <table className="table table-striped">
-          <tbody>
-          <tr scope="row">
-            <td>Bruttofortjeneste </td><td className="beloeb">{this._komma(this.props.bruttofortjeneste)}</td>
-          </tr>
-          <tr scope="row">
-            <td>Driftsresultat </td><td>{this._komma(this.props.driftsresultat)}</td>
-          </tr>
-          <tr scope="row">
-            <td>Resultat før skat </td><td>{this._komma(this.props.resultatfoerskat)}</td>
-          </tr>
-          <tr scope="row">
-            <td>Skat af årets resultat </td><td>{this._komma(this.props.skatafaaretsresultat)}</td>
-          </tr>
-          <tr scope="row">
-            <td>Årets resultat </td><td>{this._komma(this.props.aaretsresultat)}</td>
-          </tr>
-          <tr scope="row">
-            <td>Gældsforpligtelser </td><td>{this._komma(this.props.gaeldsforpligtelser)}</td>
-          </tr>
-          <tr scope="row">
-            <td>Egenkapital </td><td>{this._komma(this.props.egenkapital)}</td>
-          </tr>
-          </tbody>
-          </table>
-          <a href="#" className="btn btn-primary">Hent regnskab som PDF</a>
-        </div>
-      </div>
-    );
-  }
-
-  _komma(vaerdi) {
-    if (vaerdi) {
-      const v = vaerdi.toLocaleString();
-      return v.replace(/,/g ,".");
+    render() {
+        return(
+            <div className="card">
+              <div className="card-header" id="regnskab-header">
+                <b>{this.props.aar}</b> <span className="small">({this.props.startDato} - {this.props.slutDato})</span>
+              </div>
+              <div className="card-block">
+                <table className="table table-striped">
+                  <tbody>
+                  <tr scope="row">
+                    <td>Bruttofortjeneste </td><td className="beloeb">{this._komma(this.props.bruttofortjeneste)}</td>
+                  </tr>
+                  <tr scope="row">
+                    <td>Driftsresultat </td><td>{this._komma(this.props.driftsresultat)}</td>
+                  </tr>
+                  <tr scope="row">
+                    <td>Resultat før skat </td><td>{this._komma(this.props.resultatfoerskat)}</td>
+                  </tr>
+                  <tr scope="row">
+                    <td>Skat af årets resultat </td><td>{this._komma(this.props.skatafaaretsresultat)}</td>
+                  </tr>
+                  <tr scope="row">
+                    <td>Årets resultat </td><td>{this._komma(this.props.aaretsresultat)}</td>
+                  </tr>
+                  <tr scope="row">
+                    <td>Gældsforpligtelser </td><td>{this._komma(this.props.gaeldsforpligtelser)}</td>
+                  </tr>
+                  <tr scope="row">
+                    <td>Egenkapital </td><td>{this._komma(this.props.egenkapital)}</td>
+                  </tr>
+                  </tbody>
+                </table>
+                <a href={this.props.pdfUrl} target="_blank" className="btn btn-primary">Hent regnskab som PDF</a>
+              </div>
+            </div>
+        );
     }
 
-    return vaerdi;
-  }
+    _komma(vaerdi) {
+        if (vaerdi) {
+            const v = vaerdi.toLocaleString();
+            return v.replace(/,/g ,".");
+        }
+
+        return vaerdi;
+    }
 }
 
 
 jQuery(function() {
-  ReactDOM.render(
-    <Virksomhed />,
-    document.getElementById('react')
-  );
+    ReactDOM.render(
+        <Virksomhed />,
+        document.getElementById('react')
+    );
 })
