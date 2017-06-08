ee62b3528c00b89fdb63a6085c9ead4272f1bcb6
Rune Molin <rmo@nineconsult.dk>
2017-06-08 21:51:35 +0200

    Lille smukificering

--- frontend/src/utils/apihelper.js
+++ frontend/src/utils/apihelper.js
@@ -3,8 +3,14 @@ import 'whatwg-fetch';
 export default class APIHelper {
 
   static hentNoegletal(cvrnummer) {
-    const url = 'http://virkr.dk:9092/regnskab/' + cvrnummer;
+    return this._call('http://virkr.dk:9092/regnskab/' + cvrnummer)
+  }
 
+  static hentVirksomhedsdata(cvrnummer) {
+    return this._call('http://virkr.dk:9092/cvr/' + cvrnummer)
+  }
+
+  static _call(url) {
     return new Promise((resolve, reject) => {
       fetch(url, { code: 'cors' }).then(response => {
         if (response.ok) {
@@ -15,23 +21,6 @@ export default class APIHelper {
       }, error => {
         reject(Error(error.message))
       }
-      )
-    })
-  }
-
-  static hentVirksomhedsdata(cvrnummer) {
-    const url = 'http://virkr.dk:9092/cvr/' + cvrnummer;
-
-    return new Promise((resolve, reject) => {
-      fetch(url, { code: 'cors' }).then(response => {
-          if (response.ok) {
-            resolve(response.json())
-          } else {
-            reject(Error(response.statusText))
-          }
-        }, error => {
-          reject(Error(error.message))
-        }
       )
     })
   }
