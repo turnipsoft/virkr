50a3c00ceed4878f803e3d1448953d19cbeff541
Rune Molin <rmo@nineconsult.dk>
2017-07-09 23:52:50 +0200

    #75 Virksomhedslisten er lidt pænere
    Med inspiration fra Material Design. Dropshadow indikerer at man kan vælge
    en virksomhed

--- frontend/src/views/soegeresultat-card.js
+++ frontend/src/views/soegeresultat-card.js
@@ -7,13 +7,9 @@ const SoegeresultatCard = (props) => {
   const cvr = virksomhed.cvrNummer;
 
   return (
-    <div className="card soegeresultatcard" onClick={ () => callback(cvr) }>
-<div className="card-header">
-<span className="pull-left">{navn}</span>
-<span className="pull-right"><i className="fa fa-angle-right"></i> </span>
-
-  </div>
+    <div className="card soegeresultatcard" onClick={() => callback(cvr)}>
       <div className="card-block">
+        <h5 className="card-title">{navn}</h5>
         <div className="card-text">CVR {cvr}</div>
       </div>
     </div>
